916528cb281c639fb10eb57909172c8aa8477250
Zarahi93 <107142458+Zarahi93@users.noreply.github.com>
2022-09-02 11:56:30 -0500

    Solution and comments

---
+++ Sesión 02/helloName.js
@@ -0,0 +1,10 @@
+//**Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
+//Examples
+//helloName('Bob') → Hello Bob!
+//helloName('Alice') → Hello Alice!
+//helloName('X') → Hello X!**/
+
+function helloName(name){
+  return 'Hello ' + name + '!';//Concatenate the name to the "Hello "(minding the space between Hello and the name) 
+  //and to the "!", 
+}
